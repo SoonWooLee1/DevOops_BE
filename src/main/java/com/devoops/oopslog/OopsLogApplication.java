693711caf88ae42879756241c2a84bf0e1e1d11a
SoonWooLee1 <lsw010420@gmail.com>
2025-11-05 17:25:43 +0900

    no message

---
+++ src/main/java/com/devoops/oopslog/OopsLogApplication.java
@@ -0,0 +1,13 @@
+package com.devoops.oopslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopsLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopsLogApplication.class, args);
+    }
+
+}
